1118702ecfc6a3055d578cd87366482e4511998a
Estara <heatherjpdx@gmail.com>
2021-04-14 11:41:48 -0700

    initial commit

---
+++ part-1/index.jsx
@@ -0,0 +1,17 @@
+function App() {
+    return (
+            <div>
+                <FirstComponent/>
+                <NamedComponent name="Inigo Montoya"/>
+            </div>
+    );
+}
+
+ReactDOM.render(
+    <App/>,
+    document.getElementById("root")
+  );
+
+
+
+
---
+++ part-1/namedComponent.jsx
@@ -0,0 +1,6 @@
+function NamedComponent(name) {
+    return <p>My name is {props.name}</p>
+}
+
+ReactDOM.render(<NamedComponent/>,
+    document.getElementById("root"));
